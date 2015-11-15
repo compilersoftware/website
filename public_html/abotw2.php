beb9a23066dc534a4a101411857cfedab3272a12
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2015-11-15 23:20:57 +0100

    Add old PHP redirections

---
+++ public_html/abotw2.php
@@ -0,0 +1,3 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header('Location: /spectrum-another-brick-on-the-wall-2.html');
---
+++ public_html/abotw2_en.php
@@ -0,0 +1,3 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header('Location: /en/spectrum-another-brick-on-the-wall-2.htnl');
