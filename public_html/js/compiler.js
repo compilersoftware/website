9bed2f68b0248c4ac92ed7f85b7c0040c3c573d4
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2016-09-29 19:32:23 +0200

    Add Estar Guars HTML5 version

--- public_html/js/compiler.js
+++ public_html/js/compiler.js
@@ -1,6 +1,9 @@
 $('.modal').on('shown.bs.modal', function(e) {
     var url = '/jsemu.php?game=' + e.relatedTarget.dataset.game;
     var title = e.relatedTarget.dataset.title;
+    if (e.relatedTarget.dataset.game == 'estar_guars') {
+        url = '/EstarGuars/index.html';
+    }
     var $target = $(this);
     $target.find('iframe').attr('src', url);
     $target.find('#js-emulator-title').html(title);
