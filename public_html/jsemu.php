881804982d6694dca8704195fc52422c9dd67f20
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2019-07-30 00:25:35 +0200

    Add Dead End_ page

--- public_html/jsemu.php
+++ public_html/jsemu.php
@@ -3,6 +3,7 @@ switch ($_GET['game'])
 {
     case 'abotw2_esp' : $game = 'abotw2.es.tap'; $title="Another Brick On The Wall 2"; break;
     case 'abotw2_eng' : $game = 'abotw2.en.tap'; $title="Another Brick On The Wall 2"; break;
+    case 'dead-end_' : $game = 'dead-end_.tap'; $title="Dead End_"; break;
     case 'opp' : $game = 'opp.tap'; $title="OPP!"; break;
     case 'revelados' : $game = 'revelados.tzx'; $title="Revelados"; break;
     case 'sokoban_esp' : $game = 'sokoban.es.tzx'; $title="Sokoban"; break;
