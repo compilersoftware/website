656c1563e85571ab5106a1d1b39e0449fd2a5019
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2022-07-14 01:41:31 +0200

    fix: Remove need of PHP server

--- public_html/abotw2.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("HTTP/1.1 301 Moved Permanently");
-header('Location: /spectrum-another-brick-on-the-wall-2.html');
--- public_html/abotw2_en.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("HTTP/1.1 301 Moved Permanently");
-header('Location: /en/spectrum-another-brick-on-the-wall-2.htnl');
